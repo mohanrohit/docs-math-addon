0f7eb84e13f9adcfde1b5c99c5d48fd6ad6df44b
Rohit Mohan <rohit.mohan@gmail.com>
2017-06-30 14:55:30 -0400

    added image preview

--- MathViewModel.js
+++ MathViewModel.js
@@ -6,9 +6,27 @@ function MathViewModel()
     self.expression = ko.observable("")
     self.latex = ko.observable("");
 
+    self.imageUrl = ko.observable("");
+
+    self.getImageUrl = function()
+    {
+      return "https://latex.codecogs.com/png.latex?" + self.latex();
+    };
+
+    self.previewImage = function()
+    {
+      // get image from CodeCogs
+      self.imageUrl(self.getImageUrl());
+    };
+
+    self.insertImage = function()
+    {
+      // insert image into document
+    };
+
     self.init = function()
     {
-      var expressionInput = document.getElementById("math-expression");
+      var expressionInput = document.getElementById("formula");
 
       var mathFieldHandlers =
       {
